b052fc89f0d6afa3123cc6b2076dc233f7709f70
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2019-03-06 01:06:54 +0100

    unfinished parser refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -7,34 +7,90 @@ function createCompletionItem(name: string, body: string): CompletionItem {
 	return item;
 }
 
-class CompletionItemProviderImpl implements CompletionItemProvider {
-	provideCompletionItems(document: TextDocument): ProviderResult<CompletionItem[]> {
-		const text = document.getText();
+abstract class Parser {
+	protected abstract onWrongTag(): void;
 
-		// This thing is stateful, we don't want it to be global...
-		const snippetRegex: RegExp = /<snippet:([a-zA+Z0-9-]+)>(.*?)<\/snippet:([a-zA+Z0-9-]+)>/g;
+	private onUnknownPair(): void {
+		// TODO
+	}
 
-		const matches: RegExpMatchArray | null = text.match(snippetRegex);
+	parse(text: string): void {
+		const snippetStartRegex: RegExp = /<snippet:([a-zA+Z0-9-]+)>/g;
+		const snippetEndRegex: RegExp = /<\/snippet:([a-zA+Z0-9-]+)>/g;
 
-		if (matches) {
-			const result: CompletionItem[] = [];
+		let startMatch: RegExpExecArray | null = snippetStartRegex.exec(text);
+		
+		while (true) {
+			let endMatch: RegExpExecArray | null;
 
-			let m: RegExpExecArray | null;
-			while ((m = snippetRegex.exec(text))) {
-				const [, startName, body, stopName] = m;
+			while (true) {
+				endMatch = snippetEndRegex.exec(text);
 
-				if (startName !== stopName) {
-					window.showErrorMessage(`Local snippet: start tag != stop tag: ${startName} != ${stopName}`);
-					return [];
+				if (endMatch) {
+					if (!startMatch || startMatch.index > endMatch.index) {
+						// There is no start or start is after end, it means wrong end
+						this.onWrongTag();
+					} else {
+						// There is start and it's before end
+						break;
+					}
+				} else {
+					if (startMatch) {
+						// There no end, but there is start... just consume all starts and return
+						do {
+							this.onWrongTag();
+						} while ((startMatch = snippetStartRegex.exec(text)));
+					}
+
+					// At this point there is no end tag and all start tags are consumed
+					return
 				}
-
-				result.push(createCompletionItem(startName, body));
 			}
 
-			return result;
-		} else {
-			return [];
+			// There is a start and it's before the end
+			// What about next start?
+			let nextStartMatch: RegExpExecArray | null;
+			
+			while ((nextStartMatch = snippetStartRegex.exec(text))) {
+				if (nextStartMatch.index < endMatch.index) {
+					this.onWrongTag();
+				} else {
+					break;
+				}
+			}
+
+			this.onUnknownPair();
+
+			if (nextStartMatch) {
+				// There is a next and it is after 
+				startMatch = nextStartMatch;
+			} else {
+				// There is no next start match.... it means this is the last start
+				// How about more end tags?
+				// Because there is no start, we can just mark all of them as wrong ones
+				while ((endMatch = snippetEndRegex.exec(text))) {
+					this.onWrongTag();
+				}
+
+				return
+			}
 		}
+	}
+}
+
+class CompletionCollectingParser extends Parser {
+	completions: CompletionItem[] = [];
+
+	protected onWrongTag(): void {
+
+	}
+}
+
+class CompletionItemProviderImpl implements CompletionItemProvider {
+	provideCompletionItems(document: TextDocument): ProviderResult<CompletionItem[]> {
+		const parser = new CompletionCollectingParser();
+		parser.parse(document.getText());
+		return parser.completions;
 	}
 }
 
